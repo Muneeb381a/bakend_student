ef89eed914919a07bac6bf2ffe854daa38e02305
Muneeb381a <muneebafzal381a@gmail.com>
2024-10-28 17:40:45 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express()
+
+const port = 3000
+
+
+app.listen(port, () => {
+ console.log(`Server is Listenig at ${port}`);
+})
+
+app.get("/", (req, res) => {
+ res.json("Live Here")
+})
