fdc58fe76eaf356ac739a587ba432529d5442ba6
Muneeb381a <muneebafzal381a@gmail.com>
2024-11-06 12:02:46 +0500

    changes cors option

--- index.js
+++ index.js
@@ -21,10 +21,11 @@ const upload = multer({storage});
 
 const app = express();
 
-app.use(cors());
+
 const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({origin: "http://localhost:5173"}));
 
 const postgresPool = new Pool({
   user: process.env.DB_USER,
